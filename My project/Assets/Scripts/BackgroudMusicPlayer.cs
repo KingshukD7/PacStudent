45b576727cfa129c4665937bf5b52d60ba3ee020
Kingshuk-MAC <13983919@student.uts.edu.au>
2023-10-10 21:02:47 +1100

    Audio Clips added to Unity

---
+++ My project/Assets/Scripts/BackgroudMusicPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public AudioClip introClip;
+        public AudioClip ghostsClip;
+        
+        private AudioSource audioSource;
+    
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    
+        public void PlayMusic()
+        {
+            audioSource.clip = introClip;
+            audioSource.Play();
+            Invoke("PlayGhostsMusic", introClip.length);
+        }
+    
+        private void PlayGhostsMusic()
+        {
+            audioSource.clip = ghostsClip;
+            audioSource.Play();
+        }
+}
